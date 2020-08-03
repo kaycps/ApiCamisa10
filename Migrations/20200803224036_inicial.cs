6b7b41da9dee7d6fb1011a030ed82d5afbc7ad69
Kayc Prado <30593578+kaycps@users.noreply.github.com>
2020-08-03 19:42:01 -0300

    criação das migrations

---
+++ Migrations/20200803224036_inicial.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiCamisa10.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Levels",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Levels", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(maxLength: 45, nullable: false),
+                    email = table.Column<string>(maxLength: 45, nullable: false),
+                    password = table.Column<string>(maxLength: 45, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    dayofweak = table.Column<string>(maxLength: 25, nullable: false),
+                    hour = table.Column<string>(maxLength: 25, nullable: false),
+                    IdUser = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Teams_Users_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(maxLength: 25, nullable: false),
+                    idPosition = table.Column<int>(nullable: false),
+                    idLevel = table.Column<int>(nullable: false),
+                    idTeam = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Players_Levels_idLevel",
+                        column: x => x.idLevel,
+                        principalTable: "Levels",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Players_Positions_idPosition",
+                        column: x => x.idPosition,
+                        principalTable: "Positions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Players_Teams_idTeam",
+                        column: x => x.idTeam,
+                        principalTable: "Teams",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_idLevel",
+                table: "Players",
+                column: "idLevel");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_idPosition",
+                table: "Players",
+                column: "idPosition");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_idTeam",
+                table: "Players",
+                column: "idTeam");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_IdUser",
+                table: "Teams",
+                column: "IdUser");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Levels");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
